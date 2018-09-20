b1d00a17d2bb1ea7e46dabf43a8e08e95e5c9421
Washington Alexandre da Silva <washington.silva@SPOMM010114173.local>
2018-09-20 20:13:34 -0300

    avatar: adicionado unit test

--- src/ui/components/thf-avatar/thf-avatar.component.spec.ts
+++ src/ui/components/thf-avatar/thf-avatar.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ThfAvatarComponent } from './thf-avatar.component';
 
 describe('ThfAvatarComponent', () => {
+  const THF_AVATAR_DEFAULT_SRC = 'src/assets/images/ninja-simple-512.png';
+
   let component: ThfAvatarComponent;
   let fixture: ComponentFixture<ThfAvatarComponent>;
 
@@ -22,4 +24,27 @@ describe('ThfAvatarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update `t-src`', () => {
+    component.ngOnInit();
+
+    expect(component.src).toEqual(THF_AVATAR_DEFAULT_SRC);
+
+  });
+
+  it('should do not update `t-src`', () => {
+    const img = 'img_test';
+
+    component.src = img;
+
+    expect(component.src).toEqual(img);
+  });
+
+  it('should call `error()` component.src should be equal a THF_AVATAR_DEFAULT_SRC', () => {
+    component.onError();
+
+    expect(component.src).toEqual(THF_AVATAR_DEFAULT_SRC);
+
+  });
+
 });
